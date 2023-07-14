7cace4f388a7c3f7d9ff915deaab435942b8c67b
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-07-14 08:57:24 +0100

    Update OseDAO.sol

--- src/OseDAO.sol
+++ src/OseDAO.sol
@@ -2,4 +2,7 @@
 
 pragma solidity ^0.8.18;
 
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 contract OseDAO {}
