f0de9d0285ecef5214fff628c7a5ba4139f6fe77
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-07-14 09:05:37 +0100

    Update OseDAO.sol

--- src/OseDAO.sol
+++ src/OseDAO.sol
@@ -5,4 +5,24 @@ pragma solidity ^0.8.18;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract OseDAO {}
+contract OseDAO is ReentrancyGuard, AccessControl {
+    bytes32 public constant MEMBER = keccak256("MEMBER");
+    bytes32 public constant STAKEHOLDER = keccak256("STAKEHOLDER");
+
+    uint constant votingPeriod = 5 days;
+    uint public proposalCount;
+
+    struct Proposal {
+        uint256 id;
+        uint256 amount;
+        uint256 livePeriod;
+        uint256 voteInFavour;
+        uint256 voteAgainst;
+        string title;
+        string desciption;
+        bool isCompleted;
+        bool paid;
+        bool isPaid;
+        address payable receiverAddress;
+    }
+}
