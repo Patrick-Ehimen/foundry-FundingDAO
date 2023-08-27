ba9531a0d4cca416c92e9c907df027ccae29e6ba
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-08-27 09:15:55 +0100

    Update OseDAO.sol

--- src/OseDAO.sol
+++ src/OseDAO.sol
@@ -5,8 +5,6 @@ pragma solidity 0.8.19;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-// SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.19;
 
 /**
