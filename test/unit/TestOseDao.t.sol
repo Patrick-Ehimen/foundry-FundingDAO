bbb5e3023ba97d729fb6e0e7b7440e811f3336f2
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-08-28 10:21:10 +0100

    Update TestOseDao.t.sol

--- test/unit/TestOseDao.t.sol
+++ test/unit/TestOseDao.t.sol
@@ -16,14 +16,14 @@ contract TestOseDao is Test {
         payable(makeAddr("receiverAddressForFunds"));
 
     uint public constant STARTING_BALANCE = 10 ether;
-    uint public constant STARTING_DAO_BALANCE = 1000 ether;
+    uint public constant STARTING_DAO_BALANCE = 3000 ether;
     uint public constant MIN_DEPOSIT_VALUE = 1 ether;
     uint public constant DEPOSIT_VALUE = 2 ether;
 
     function setUp() public {
-        vm.startPrank(owner);
+        // vm.startPrank(owner);
         oseDaoContract = new OseDAO();
-        vm.stopPrank();
+        // vm.stopPrank();
 
         console.log("address of contract", address(oseDaoContract));
         console.log("address of owner", address(owner));
@@ -42,9 +42,9 @@ contract TestOseDao is Test {
      * @notice Remember to write a script to programmatically send funds.
      */
     function testCreateStakeHolder() public payable {
-        // uint256 amount = msg.value; //@audit remember to write script to programatically send funds
+        uint256 amount = msg.value; //@audit remember to write script to programatically send funds
 
-        uint256 amount = 2 ether;
+        // uint256 amount = 2 ether;
 
         vm.startPrank(nonMember);
         oseDaoContract.createStakeholderAndMember{value: amount}();
@@ -74,13 +74,34 @@ contract TestOseDao is Test {
         vm.stopPrank();
     }
 
-    // function testCreateProposal() public {
-    //     string memory title = "New Proposal";
-    //     string memory desc = "This is a new proposal";
-    //     address receiverAddress = receiverAddressForFunds;
-    //     uint256 amount = 100;
-    //     string memory imageId = "123";
-    // }
+    function testCreateProposalproposal() public {
+        vm.startPrank(nonMember);
+
+        oseDaoContract.createStakeholderAndMember{value: 2 ether}();
+        string memory title = "New Proposal";
+        string memory desc = "This is a new proposal";
+        address receiverAddress = receiverAddressForFunds;
+        uint256 amount = 30 ether;
+        string memory imageId = "123";
+
+        oseDaoContract.createProposal{value: 5e18}(
+            title,
+            desc,
+            receiverAddress,
+            amount,
+            imageId
+        );
+
+        Proposal memory proposal = oseDaoContract.getProposal(0);
+
+        assertEq(proposal.title, title);
+        assertEq(proposal.description, description);
+        assertEq(proposal.receiverAddress, receiver);
+        assertEq(proposal.amount, amount);
+        assertEq(proposal.imageId, imageId);
+
+        vm.stopPrank();
+    }
 
     //Test balance retrieval for stakeholders and members.
     function testRetrieveBalances() public {
